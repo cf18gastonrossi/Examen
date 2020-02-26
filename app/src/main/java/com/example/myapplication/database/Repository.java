fa8c710b5ad6a278fb3ffd3cc14327ec32770231
Gastón Rossi <gaston.rossi@basetis.com>
2020-02-26 20:43:02 +0100

    Repo

--- app/src/main/java/com/example/myapplication/database/Repository.java
+++ app/src/main/java/com/example/myapplication/database/Repository.java
@@ -1,4 +1,29 @@
 package com.example.myapplication.database;
 
-public class Repository {
+import android.app.Application;
+
+
+import com.example.myapplication.Model.Event;
+import com.example.fichapp.data.RoomConnection;
+
+import java.util.ArrayList;
+import com.example.timeapp.Dao.EventDao;
+
+public class Repository{
+
+    static EventDao eventDao;
+
+    public Repository(Application application) {
+        RoomConnection fichappRoomDb = RoomConnection.getDatabase(application.getApplicationContext());
+        eventDao = fichappRoomDb.eventRoom();
+    }
+
+    public static void registerNewEvent(final Event event) {
+            eventDao.insertEvent(event);
+    }
+
+    public static ArrayList<Event> getEvents() { return Repository.getEvents();
+    }
+
+
 }
